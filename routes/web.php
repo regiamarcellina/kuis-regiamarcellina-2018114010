0c31cd0fa8e9dc4cfb4b196612b8d111433d9148
regiamarcellina <regiamarcelina@gmail.com>
2021-01-30 16:46:02 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\AbsenController;
+use Illuminate\Support\Facades\Route;
+use App\Http\controllers\AppController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\MatkulController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('', [AppController::class, 'index']);
+
+Route::resources([
+    'absen' => AbsenController::class,
+    'matkul' => MatkulController::class,
+    'mahasiswa' => MahasiswaController::class
+]);
